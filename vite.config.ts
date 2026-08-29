1051664e747ed6bb529a6c1908ba6ac717575749
gtl945 <2431967868@qq.com>
2026-08-29 21:51:50 +0800

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  envPrefix: ["VITE_", "TAURI_"],
+});
