5e44fd00480239786be8fc61c1a252958e1bb188
gtl945 <2431967868@qq.com>
2026-08-29 23:45:40 +0800

    Update src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import ImportView from "@/views/ImportView.vue";
 import KnowledgeBaseView from "@/views/KnowledgeBaseView.vue";
 import ExportView from "@/views/ExportView.vue";
+import DatabaseView from "@/views/DatabaseView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,6 +11,7 @@ const router = createRouter({
     { path: "/import", component: ImportView },
     { path: "/kb", component: KnowledgeBaseView },
     { path: "/export", component: ExportView },
+    { path: "/database", component: DatabaseView },
   ],
 });
 
